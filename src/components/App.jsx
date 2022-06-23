4139e7088112f97e78530eea389bbe5c2e9f89ae
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-06-23 23:57:19 +0300

    всі запити

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
-import { fetchTrending } from '../services/API';
+import {
+  fetchTrending,
+  fetchSearch,
+  fetchMovieDetails,
+  fetchMovieCredits,
+  fetchMovieReviews,
+} from '../services/API';
 
 export const App = () => {
   fetchTrending().then(res => console.log(res));
+  fetchSearch('super').then(res => console.log(res));
+  fetchMovieDetails('641501').then(res => console.log(res));
+  fetchMovieCredits('746333').then(res => console.log(res));
+  fetchMovieReviews('913205').then(res => console.log(res));
+
   return <div>123</div>;
 };
--- src/services/API.js
+++ src/services/API.js
@@ -4,6 +4,38 @@ const key = '6169d862ccc0d011b31a05667d9a5c10';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3'
 
 export async function fetchTrending() {
-    const { data } = await axios.get(`/trending/movie/day?api_key=${key}&page=1`);
+    const { data } = await axios.get(`/trending/movie/day?api_key=${key}&language=uk&page=1`);
     return data;
 }
+
+export async function fetchSearch(query) {
+    const { data } = await axios.get(`/search/movie?api_key=${key}&query=${query}&language=uk&page=1`);
+    return data;
+}
+
+export async function fetchMovieDetails(movieId) {
+    const { data } = await axios.get(`/movie/${movieId}?api_key=${key}&language=uk`);
+    return data;
+}
+
+export async function fetchMovieCredits(movieId) {
+    const { data } = await axios.get(`/movie/${movieId}/credits?api_key=${key}&language=uk`);
+    return data;
+}
+
+export async function fetchMovieReviews(movieId) {
+    const { data } = await axios.get(`/movie/${movieId}/reviews?api_key=${key}&language=uk&page=1`);
+    return data;
+}
+
+
+
+
+
+
+
+// /trending/get - trending список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+// / search / search - movies пошук фільму за ключовим словом на сторінці фільмів.
+// / movies / get - movie - details запит повної інформації про фільм для сторінки кінофільму.
+// / movies / get - movie - credits запит інформації про акторський склад для сторінки кінофільму.
+// / movies / get - movie - reviews запит оглядів для сторінки кінофільму.
